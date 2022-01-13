1dc34f2490dc49dd05ce76b746746892c36ef855
João Victor <victoralves169@outlook.com>
2022-01-13 10:19:14 -0300

    tentanco atribuir JOptionPane

--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 /**
  *
